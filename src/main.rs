4cbf50fd65250724628b39e6ab271777c2be8c51
Julien Chapron <jc271200001@protonmail.com>
2022-07-21 14:23:20 +0200

    add cargo-watch + operator calculs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+
+    // addition entier
+    let mut mon_entier_addition : i32 = 42;
+    mon_entier_addition = mon_entier_addition + 2;
+    dbg!(mon_entier_addition);
+
+    // soustraction entier
+    let mut mon_entier_soustraction : i32 = 42;
+    mon_entier_soustraction = mon_entier_soustraction - 2;
+    dbg!(mon_entier_soustraction);
+
+    // division euclidienne entier
+    let mut mon_entier_division : i32 = 42;
+    mon_entier_division = mon_entier_division / 4;
+    dbg!(mon_entier_division);
+
+    // modulo entier
+    let mut mon_entier_division : i32 = 42;
+    mon_entier_division = mon_entier_division % 10;
+    dbg!(mon_entier_division);
+
 }
